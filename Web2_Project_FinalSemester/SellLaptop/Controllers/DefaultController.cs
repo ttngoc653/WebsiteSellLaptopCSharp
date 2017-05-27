2a6bc696d369471d858f04c6be19558df59ba108
Tran The Ngoc <1460653@student.hcmus.edu.vn>
2017-05-27 17:40:31 +0700

    xử lý tìm kiếm

--- Web2_Project_FinalSemester/SellLaptop/Controllers/DefaultController.cs
+++ Web2_Project_FinalSemester/SellLaptop/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SellLaptop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,12 +9,84 @@ namespace SellLaptop.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
-        public ActionResult Index()
+        [ChildActionOnly]
+        public ActionResult Search() { return PartialView(); }
+             
+        [ChildActionOnly]
+        public ActionResult SearchListHangSX()
         {
-            int i = 0;
-            return View();
+            using (var ent=new sellLaptopEntities())
+            {
+                List<hang_sx> l = ent.hang_sx.ToList();
+                return PartialView(l);
+            }
+            return PartialView();
+        }
 
+        [ChildActionOnly]
+        public ActionResult SearchGiaCa() { return PartialView(); }
+
+        [ChildActionOnly]
+        public ActionResult SearchDoHoa()
+        {
+            using (var ent = new sellLaptopEntities())
+            {
+                List<cart_do_hoa> l = ent.cart_do_hoa.OrderBy(x =>x.thietke).ToList();
+                String tam = "";
+                for (int i = 0; i < l.Count; i++)
+                {
+                    if (tam == l[i].thietke)
+                    {
+                        l.RemoveAt(i);
+                        i--;
+                    }
+                    else tam = l[i].thietke;
+                }
+                return PartialView(l);
+            }
+            return PartialView();
+        }
+        
+        [ChildActionOnly]
+        public ActionResult SearchDungLuongRam()
+        {
+            using (var ent = new sellLaptopEntities())
+            {
+                List<san_pham> l = ent.san_pham.OrderBy(x => x.ramdl).ToList();
+                int tam = 0;
+                for (int i = 0; i < l.Count; i++)
+                {
+                    if (tam == l[i].ramdl)
+                    {
+                        l.RemoveAt(i);
+                        i--;
+                    }
+                    else tam = l[i].ramdl;
+                }
+                return PartialView(l);
+            }
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult SearchCPU()
+        {
+            using (var ent = new sellLaptopEntities())
+            {
+                List<cpu> l = ent.cpus.OrderBy(x => x.congnghe).ToList();
+                String tam = "";
+                for (int i = 0; i < l.Count; i++)
+                {
+                    if (tam == l[i].congnghe)
+                    {
+                        l.RemoveAt(i);
+                        i--;
+                    }
+                    else tam = l[i].congnghe;
+                }
+                return PartialView(l);
+            }
+            return PartialView();
         }
     }
 }
